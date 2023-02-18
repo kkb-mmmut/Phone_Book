c75caabcc204b3b26ffee102cd67dfe005290ef0
Kamlesh Kumar Bind <kkb.mmmut@gmail.com>
2023-02-18 08:23:40 +0530

    Updates

---
+++ precache-manifest.5b6884a83b7e45f631f7acb6cc4f161b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c44678793af0d27ae28a9e9836daeef",
+    "url": "/Phone_Book/index.html"
+  },
+  {
+    "revision": "af99c5d97397ce3b0f24",
+    "url": "/Phone_Book/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "030aef5284d9577b5e16",
+    "url": "/Phone_Book/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "af99c5d97397ce3b0f24",
+    "url": "/Phone_Book/static/js/2.fb1d6f27.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Phone_Book/static/js/2.fb1d6f27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "030aef5284d9577b5e16",
+    "url": "/Phone_Book/static/js/main.67062cdf.chunk.js"
+  },
+  {
+    "revision": "a7c14bb4fae2f03c7390",
+    "url": "/Phone_Book/static/js/runtime-main.cb2b9203.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Phone_Book/static/media/logo.5d5d9eef.svg"
+  }
+]);
